4340f40bbd3ec90757330a90be5290f623df6f33
andiccc <59291754+andiccc@users.noreply.github.com>
2021-04-10 04:32:02 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ radio.onReceivedNumber(function (receivedNumber) {
         basic.pause(1000)
         radio.sendNumber(temp)
     }
-    if (id == receivedNumber) {
+    if (id2 == receivedNumber) {
         radio.sendString(CAR_2)
         basic.pause(1000)
         radio.sendNumber(temp)
@@ -21,7 +21,7 @@ radio.onReceivedString(function (receivedString) {
         basic.pause(1000)
     }
     if (CAR_2 == receivedString) {
-        basic.showString("CAR 1")
+        basic.showString("CAR 2")
         basic.pause(1000)
         basic.showString("Temp")
         basic.showNumber(temp)
@@ -29,17 +29,24 @@ radio.onReceivedString(function (receivedString) {
     }
 })
 let CAR_2 = ""
+let id2 = 0
 let CAR_1 = ""
 let id = 0
 let temp = 0
 temp = input.temperature()
 id = 0
 basic.forever(function () {
-    if (input.isGesture(Gesture.LogoDown)) {
+    if (input.isGesture(Gesture.ScreenUp)) {
         id = 5
     }
     if (id == 5) {
         radio.sendNumber(id)
+    }
+    if (input.isGesture(Gesture.ScreenDown)) {
+        id = 6
+    }
+    if (id == 6) {
+        radio.sendNumber(id2)
     }
 })
 basic.forever(function () {
